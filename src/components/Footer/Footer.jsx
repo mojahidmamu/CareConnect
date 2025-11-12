4efe594b550b38371b6ee57db862cb2c3a0dbe08
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-11-12 19:24:26 +0600

    added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10 px-32 justify-between mt-10">
       <div>
-        <p>This is footer...</p>
+        <p>This is footer...Hello Abdullah all Mojahid...</p>
       </div>
     </footer>
   );
