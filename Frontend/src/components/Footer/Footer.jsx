cdbbadc9d9587b01e0d6c197839275bcd4c0b816
pritom-dey1 <Pritom1.2.zx@gmail.com>
2025-11-12 14:36:38 +0600

    added drf create apis for part one

---
+++ Frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10 px-32 justify-between mt-10">
+      <div>
+        <p>This is footer...</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
